32cc77b0daa9e3f6fce192351ad6a5c931513e4f
Kabita Chongbang <kabitachongbang786@gmail.com>
2019-04-18 00:23:50 +0800

    update db url based on env variable

--- Routes/api/formData.js
+++ Routes/api/formData.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
 
-const myUrl = "mongodb://localhost:27017/students";
+const myUrl = process.env.DB_URL || 'mongodb://localhost:27017/students';
 
 let studentCollection;
 
