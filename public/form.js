63971807d6b6460325c3849cff58f36ed48b7b02
Kabita Chongbang <kabitachongbang786@gmail.com>
2019-04-28 22:42:34 +0800

    Updated JavaScript

--- public/form.js
+++ public/form.js
@@ -1,146 +1,144 @@
-(function () {
-    'use strict';
+(function() {
+  "use strict";
 
-    angular.module('expressForm', []);
-    angular.module('expressForm').
-        controller('MainController', MainController);
+  angular.module("expressForm", []);
+  angular.module("expressForm").controller("MainController", MainController);
 
-    MainController.$inject = ['$scope', 'enrollStudent'];
-    function MainController($scope, enrollStudent) {
-        $scope.student = {};
-        $scope.students = [];
+  MainController.$inject = ["$scope", "enrollStudent"];
+  function MainController($scope, enrollStudent) {
+    $scope.student = {};
+    $scope.students = [];
 
-        loadStudents();
-        $scope.submitForm = function () {
-            //clear form styles upon submitting form
-            $scope.studentForm.$setPristine();
+    loadStudents();
+    $scope.submitForm = function() {
+      //clear form styles upon submitting form
+      $scope.studentForm.$setPristine();
 
-            if ($scope.student._id === null || $scope.student._id === undefined) {
-
-                enrollStudent.saveUsers($scope.student).then(function (result) {
-                    console.log('result is', result);
-                    clearForm();
-                    loadStudents();
-                });
-            }
-            else {
-                enrollStudent.updateSingleStudent($scope.student._id, $scope.student).then(function (result) {
-                    loadStudents();
-                    console.log('data updated', result);
-                    clearForm();
-                });
-            }
-
-        };
-
-        $scope.deleteSingleUser = function (userId) {
-            enrollStudent.removeSingleStudent(userId).then(function (result) {
-                console.log('result after delete', result);
-                loadStudents();
-            });
-        };
-
-        $scope.updateSingleUser = function (user) {
-            console.log('user value', user);
-            $scope.student = user;
-
-        };
-
-        function loadStudents() {
-            enrollStudent.getAll().then(function (result) {
-                $scope.students = result;
-            });
-        }
-
-        function clearForm() {
-            $scope.student = {};
-        }
-
-        $scope.propertyName = 'name';
-        $scope.reverse = false;
-        $scope.sortBy = function (propertyName) {
-            $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : $scope.reverse;
-            $scope.propertyName = propertyName;
-        };
-
+      if ($scope.student._id === null || $scope.student._id === undefined) {
+        enrollStudent.saveUsers($scope.student).then(function(result) {
+          console.log("result is", result);
+          clearForm();
+          loadStudents();
+        });
+      } else {
+        enrollStudent
+          .updateSingleStudent($scope.student._id, $scope.student)
+          .then(function(result) {
+            loadStudents();
+            console.log("data updated", result);
+            clearForm();
+          });
+      }
     };
 
-    angular.module('expressForm')
-        .factory('enrollStudent', function ($http) {
-            const BASE_URL = 'https://kabita-student-enrollment.herokuapp.com/api/formData/';
+    $scope.deleteSingleUser = function(userId) {
+      enrollStudent.removeSingleStudent(userId).then(function(result) {
+        console.log("result after delete", result);
+        loadStudents();
+      });
+    };
 
-            function getAll() {
-                return $http({
-                    method: 'GET',
-                    url: BASE_URL
-                }).then(function (result) {
-                    return result.data;
-                });
+    $scope.updateSingleUser = function(user) {
+      console.log("user value", user);
+      $scope.student = user;
+    };
 
-            }
+    function loadStudents() {
+      enrollStudent.getAll().then(function(result) {
+        $scope.students = result;
+      });
+    }
 
-            function saveUsers(user) {
+    function clearForm() {
+      $scope.student = {};
+    }
 
-                return $http({
-                    method: 'POST',
-                    url: BASE_URL,
-                    data: user
-                })
-                    .then(function (successResult) {
-                        return successResult;
-                    },
-                        function (failedResult) {
-                            console.log('failedResult', failedResult);
-                        });
-            }
+    $scope.propertyName = "name";
+    $scope.reverse = false;
+    $scope.sortBy = function(propertyName) {
+      $scope.reverse =
+        $scope.propertyName === propertyName ? !$scope.reverse : $scope.reverse;
+      $scope.propertyName = propertyName;
+    };
+  }
 
+  angular.module("expressForm").factory("enrollStudent", function($http) {
+    const BASE_URL =
+      "https://kabita-student-enrollment.herokuapp.com/api/formData/";
 
-            function removeSingleStudent(userId) {
-                return $http({
-                    method: 'DELETE',
-                    url: 'http://localhost:3000/api/formData/' + userId
-                })
-                    .then(function (successResult) {
-                        console.log('successResult', successResult);
-                    },
-                        function (failedResult) {
-                            return failedResult;
-                        });
-            }
+    function getAll() {
+      return $http({
+        method: "GET",
+        url: BASE_URL
+      }).then(function(result) {
+        return result.data;
+      });
+    }
 
-            function getSingleStudent(userId) {
-                return $http({
-                    method: 'GET',
-                    url: 'http://localhost:3000/api/formData/' + userId
-                })
-                    .then(function (successResult) {
-                        return successResult;
-                    },
-                        function (failedResult) {
-                            return failedResult;
-                        });
-            }
+    function saveUsers(user) {
+      return $http({
+        method: "POST",
+        url: BASE_URL,
+        data: user
+      }).then(
+        function(successResult) {
+          return successResult;
+        },
+        function(failedResult) {
+          console.log("failedResult", failedResult);
+        }
+      );
+    }
 
-            function updateSingleStudent(userId, user) {
-                return $http({
-                    method: 'PUT',
-                    url: 'http://localhost:3000/api/formData/' + userId,
-                    data: user
-                })
-                    .then(function (successResult) {
-                        return successResult;
-                    },
-                        function (failedResult) {
-                            return failedResult;
-                        })
-            }
+    function removeSingleStudent(userId) {
+      return $http({
+        method: "DELETE",
+        url: "http://localhost:3000/api/formData/" + userId
+      }).then(
+        function(successResult) {
+          console.log("successResult", successResult);
+        },
+        function(failedResult) {
+          return failedResult;
+        }
+      );
+    }
 
-            return {
-                getAll: getAll,
-                saveUsers: saveUsers,
-                removeSingleStudent: removeSingleStudent,
-                getSingleStudent: getSingleStudent,
-                updateSingleStudent: updateSingleStudent
-            }
-        });
+    function getSingleStudent(userId) {
+      return $http({
+        method: "GET",
+        url: "http://localhost:3000/api/formData/" + userId
+      }).then(
+        function(successResult) {
+          return successResult;
+        },
+        function(failedResult) {
+          return failedResult;
+        }
+      );
+    }
+
+    function updateSingleStudent(userId, user) {
+      return $http({
+        method: "PUT",
+        url: "http://localhost:3000/api/formData/" + userId,
+        data: user
+      }).then(
+        function(successResult) {
+          return successResult;
+        },
+        function(failedResult) {
+          return failedResult;
+        }
+      );
+    }
+
+    return {
+      getAll: getAll,
+      saveUsers: saveUsers,
+      removeSingleStudent: removeSingleStudent,
+      getSingleStudent: getSingleStudent,
+      updateSingleStudent: updateSingleStudent
+    };
+  });
 })();
